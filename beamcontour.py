6c93d71c5ffe28de4eeeb1b103e95c534adf3f8b
Evan Mayer <contactevanmayer@gmail.com>
2017-02-26 19:17:13 -0600

    removed extranous info

--- beamcontour.py
+++ beamcontour.py
@@ -9,10 +9,9 @@ import numpy as np
 from scipy import loadtxt
 import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d.axes3d as p3
-# from rotanimate import *
 
 phi, theta, db = (
-                  loadtxt("D:/Google Drive/UChicago/PHYS 29X00/Winter/idealbeam.csv", 
+                  loadtxt("filename.csv", 
                           delimiter = ",", unpack=True, skiprows=1))
 xsize = np.argmax(phi)+1
 ysize = max(theta) - min(theta) + 1
@@ -34,9 +33,3 @@ ax.set_xlabel('X')
 ax.set_xlabel('Y')
 ax.plot_surface(x,y,z, rstride=3, cstride=6, cmap=plt.get_cmap('jet'))
 plt.show()
-
-# angles = np.linspace(0,360,21)[:-1] # A list of 20 angles between 0 and 360
- 
-# create an animated gif (20ms between frames)
-# rotanimate(ax, angles,'D:/Google Drive/UChicago/PHYS 29X00/Winter/movie.gif',delay=20)
-
